74fbd1fd62f8851ed78c78a78f49cbc580c16216
fexla <41383116+fexla@users.noreply.github.com>
2023-10-18 15:53:19 +0800

    rotation

---
+++ src/math/Rotation.h
@@ -0,0 +1,41 @@
+//
+// Created by q on 2023/10/18.
+//
+
+#ifndef MYSOFTWARERENDERPIPE_ROTATION_H
+#define MYSOFTWARERENDERPIPE_ROTATION_H
+
+#include "Matrix.h"
+#include "renderer_math.h"
+
+class EulerAngle {
+public:
+    float &roll() {
+        return x;
+    }
+
+    float &pitch() {
+        return y;
+    }
+
+    float &yaw() {
+        return z;
+    }
+
+    Matrix4x4 rotationMatrix() {
+        float s3 = sin(x);
+        float s2 = sin(y);
+        float s1 = sin(z);
+        float c3 = cos(x);
+        float c2 = cos(y);
+        float c1 = cos(z);
+        return Matrix4x4{c1 * c2, c1 * s2 * s3 - c3 * s1, s1 * s3 + c1 * c3 * s2, 0,
+                         c2 * s1, c1 * c3 + s1 * s2 * s3, c3 * s1 * s2 - c1 * s3, 0,
+                         -s2, c2 * s3, c2 * c3, 0,
+                         0, 0, 0, 1};
+    }
+
+    float x, y, z;
+};
+
+#endif //MYSOFTWARERENDERPIPE_ROTATION_H
---
+++ test/test02_linear_algebra/test_rotation.cpp
@@ -0,0 +1,16 @@
+//
+// Created by q on 2023/10/18.
+//
+#include "Rotation.h"
+#include "iostream"
+
+
+int main() {
+    Vector4f pos1{1, 0, 0, 1};
+    std::cout << "将" << pos1 << "绕x轴旋转45度:" << std::endl;
+    std::cout << EulerAngle{45.0 / 180 * 3.1415926, 0, 0}.rotationMatrix() * pos1;
+    std::cout << "将" << pos1 << "绕y轴旋转45度:" << std::endl;
+    std::cout << EulerAngle{0,45.0 / 180 * 3.1415926,  0}.rotationMatrix() * pos1.asMatrix();
+    std::cout << "将" << pos1 << "绕z轴旋转45度:" << std::endl;
+    std::cout << EulerAngle{ 0, 0,45.0 / 180 * 3.1415926}.rotationMatrix() * pos1.asMatrix();
+}
