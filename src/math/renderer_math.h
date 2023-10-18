56e6a3d6c89776b4ad1ef4ee5105a23971a41548
fexla <41383116+fexla@users.noreply.github.com>
2023-10-18 20:21:19 +0800

    add definition of vectorni

--- src/math/renderer_math.h
+++ src/math/renderer_math.h
@@ -12,6 +12,9 @@
 using Vector2f = mvector<float, 2>;
 using Vector3f = mvector<float, 3>;
 using Vector4f = mvector<float, 4>;
+using Vector2i = mvector<int, 2>;
+using Vector3i = mvector<int, 3>;
+using Vector4i = mvector<int, 4>;
 using Matrix3x3 = matrix<float, 3, 3>;
 using Matrix4x4 = matrix<float, 4, 4>;
 
