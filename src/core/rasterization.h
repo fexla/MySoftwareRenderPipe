f5fa302d13e02f4dbd7a191327f7a97e28ab5402
fexla <41383116+fexla@users.noreply.github.com>
2023-10-19 14:59:48 +0800

    rasterization of line

--- src/core/rasterization.h
+++ src/core/rasterization.h
@@ -15,4 +15,73 @@ rasterize_point(GraphicsBuffer *buffer,
                 Shader &shader) {
     (*buffer)[pos[0]][pos[1]] = shader.shade();
 }
+
+/**
+ * Bresenham画线算法
+ * @tparam GraphicsBuffer 画布类型
+ * @tparam VertexData 顶点数据类型
+ * @tparam Shader 着色器
+ * @param buffer 画布
+ * @param start 起点坐标
+ * @param end 终点坐标
+ * @param v1Data 起点数据
+ * @param v2Data 终点数据
+ * @param shader 着色器
+ * @return void
+ */
+template<typename GraphicsBuffer, typename VertexData, typename Shader>
+std::enable_if_t<std::is_base_of_v<line_frag_shader<VertexData>, Shader>, void>
+rasterize_line(GraphicsBuffer *buffer,
+               Vector2i start,
+               Vector2i end,
+               const VertexData *v1Data,
+               const VertexData *v2Data,
+               Shader &shader) {
+    if (start == end) {
+        (*buffer)[start[0]][start[1]] = shader.shade(v1Data, v2Data, 1);
+        return;
+    }
+    //计算起点终点在x轴和y轴上的距离
+    int xDiff = abs(end[0] - start[0]);
+    int yDiff = abs(end[1] - start[1]);
+    if (yDiff <= xDiff) {
+        bool bFlip = 0;
+        float t = 0;
+        if (start[0] > end[0]) {
+            std::swap(start, end);
+            bFlip = 1;
+            t = 1;
+        }
+        float k = 1.0 * (end[1] - start[1]) / xDiff;
+        float xDiffInv = 1.0 / xDiff;
+        for (int i = 0; i <= xDiff; ++i) {
+            int y = start[1] + i * k + 0.5;
+            (*buffer)[start[0] + i][y] = shader.shade(v1Data, v2Data, t);
+            if (bFlip) {
+                t -= xDiffInv;
+            } else {
+                t += xDiffInv;
+            }
+        }
+    } else {
+        bool bFlip = 0;
+        float t = 0;
+        if (start[1] > end[1]) {
+            std::swap(start, end);
+            bFlip = 1;
+            t = 1;
+        }
+        float k = 1.0 * (end[0] - start[0]) / yDiff;
+        float yDiffInv = 1.0 / yDiff;
+        for (int i = 0; i <= yDiff; ++i) {
+            int x = start[0] + i * k + 0.5;
+            (*buffer)[x][start[1] + i] = shader.shade(v1Data, v2Data, t);
+            if (bFlip) {
+                t -= yDiffInv;
+            } else {
+                t += yDiffInv;
+            }
+        }
+    }
+}
 #endif //MYSOFTWARERENDERPIPE_RASTERIZATION_H
---
+++ test/test04_rasterization/test_rasterize_line.cpp
@@ -0,0 +1,99 @@
+//
+// Created by q on 2023/10/19.
+//
+#include "GLFW/glfw3.h"
+#include "renderer_math.h"
+#include "color.h"
+#include "rasterization.h"
+
+const int W = 640;
+const int H = 480;
+
+char buffer[W * H * 3];
+
+template<typename Graphic_Buffer>
+void refresh(Graphic_Buffer *graphicBuffer) {
+    for (int i = 0; i < W; ++i) {
+        for (int j = 0; j < H; ++j) {
+            buffer[(j * W + i) * 3] = (*graphicBuffer)[i][j][0] * 255;
+            buffer[(j * W + i) * 3 + 1] = (*graphicBuffer)[i][j][1] * 255;
+            buffer[(j * W + i) * 3 + 2] = (*graphicBuffer)[i][j][2] * 255;
+        }
+    }
+}
+
+class shader_lerp_color : public line_frag_shader<color> {
+public:
+    color shade(const color *v1, const color *v2, float t) {
+        return {
+                v1->r * (1 - t) + v2->r * t,
+                v1->g * (1 - t) + v2->g * t,
+                v1->b * (1 - t) + v2->b * t,
+                v1->a * (1 - t) + v2->a * t,
+        };
+    }
+};
+
+int main(void) {
+    GLFWwindow *window;
+
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(W, H, "Hello World", NULL, NULL);
+    if (!window) {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    auto a = new matrix<color, W, H>;
+    line_frag_shader<color> shader;
+    shader_lerp_color shader_lerp;
+
+    rasterize_line(a, Vector2i{W / 2 - 40 - 100, H / 2 + 100}, Vector2i{W / 2 + 40 - 100, H / 2 + 100},
+                   &C_WHITE, &C_BLACK, shader);
+    rasterize_line(a, Vector2i{W / 2 - 100, H / 2 - 40 + 100}, Vector2i{W / 2 - 100, H / 2 + 40 + 100},
+                   &C_WHITE, &C_BLACK, shader);
+    rasterize_line(a, Vector2i{W / 2 - 40 - 100, H / 2 - 40}, Vector2i{W / 2 + 40 - 100, H / 2 + 40},
+                   &C_WHITE, &C_BLACK, shader);
+    rasterize_line(a, Vector2i{W / 2 - 40 - 100, H / 2 + 40}, Vector2i{W / 2 + 40 - 100, H / 2 - 40},
+                   &C_WHITE, &C_BLACK, shader);
+    rasterize_line(a, Vector2i{W / 2 - 40 - 100, H / 2 - 60 - 100}, Vector2i{W / 2 + 40 - 100, H / 2 + 60 - 100},
+                   &C_WHITE, &C_BLACK, shader);
+    rasterize_line(a, Vector2i{W / 2 - 40 - 100, H / 2 + 60 - 100}, Vector2i{W / 2 + 40 - 100, H / 2 - 60 - 100},
+                   &C_WHITE, &C_BLACK, shader);
+
+    int pointNum = 50;
+    float lineLen = 80;
+    for (int i = 0; i < pointNum; ++i) {
+        float angle = 1.0 * i / pointNum * M_PI * 2;
+        Vector2i pos{(int) (W / 2 + 100 + lineLen * cos(angle)),
+                     (int) (H / 2 + lineLen * sin(angle))};
+        rasterize_line(a, {W / 2 + 100, H / 2}, pos, &C_RED, &C_Blue, shader_lerp);
+    }
+
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window)) {
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        refresh(a);
+
+        glDrawPixels(W, H, GL_RGB, GL_UNSIGNED_BYTE, buffer);
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
