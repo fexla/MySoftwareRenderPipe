4c5680091d2332f51518a1856b405843c186ea4e
fexla <41383116+fexla@users.noreply.github.com>
2023-10-19 14:01:25 +0800

    fix the issue of not automatically inferring shader types

--- src/core/rasterization.h
+++ src/core/rasterization.h
@@ -9,9 +9,10 @@
 #include "shader.h"
 
 template<typename GraphicsBuffer, typename Shader=point_shader>
-inline void rasterize_point(GraphicsBuffer *buffer,
-                            Vector2i pos,
-                            std::enable_if_t<std::is_base_of_v<point_shader, Shader>, Shader> &shader) {
+inline std::enable_if_t<std::is_base_of_v<point_shader, Shader>, void>
+rasterize_point(GraphicsBuffer *buffer,
+                Vector2i pos,
+                Shader &shader) {
     (*buffer)[pos[0]][pos[1]] = shader.shade();
 }
 #endif //MYSOFTWARERENDERPIPE_RASTERIZATION_H
