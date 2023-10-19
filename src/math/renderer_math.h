6017f337203e5311387ca651785d7532ff93de6b
fexla <41383116+fexla@users.noreply.github.com>
2023-10-19 17:36:53 +0800

    rm_type_names.h

--- src/math/renderer_math.h
+++ src/math/renderer_math.h
@@ -8,15 +8,7 @@
 #include "matrix.h"
 #include "mvector.h"
 #include "rotation.h"
-
-using Vector2f = mvector<float, 2>;
-using Vector3f = mvector<float, 3>;
-using Vector4f = mvector<float, 4>;
-using Vector2i = mvector<int, 2>;
-using Vector3i = mvector<int, 3>;
-using Vector4i = mvector<int, 4>;
-using Matrix3x3 = matrix<float, 3, 3>;
-using Matrix4x4 = matrix<float, 4, 4>;
+#include "rm_type_names.h"
 
 
 #endif //MYSOFTWARERENDERPIPE_RENDERER_MATH_H
---
+++ src/math/rm_type_names.h
@@ -0,0 +1,20 @@
+//
+// Created by q on 2023/10/19.
+//
+
+#ifndef MYSOFTWARERENDERPIPE_RM_TYPE_NAMES_H
+#define MYSOFTWARERENDERPIPE_RM_TYPE_NAMES_H
+
+#include "mvector.h"
+
+
+using Vector2f = mvector<float, 2>;
+using Vector3f = mvector<float, 3>;
+using Vector4f = mvector<float, 4>;
+using Vector2i = mvector<int, 2>;
+using Vector3i = mvector<int, 3>;
+using Vector4i = mvector<int, 4>;
+using Matrix3x3 = matrix<float, 3, 3>;
+using Matrix4x4 = matrix<float, 4, 4>;
+
+#endif //MYSOFTWARERENDERPIPE_RM_TYPE_NAMES_H
