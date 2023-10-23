da0a4e928a53bb87a26dc301dbb0bba445ced196
fexla <41383116+fexla@users.noreply.github.com>
2023-10-23 19:20:21 +0800

    light

---
+++ src/core/light.h
@@ -0,0 +1,15 @@
+//
+// Created by q on 2023/10/23.
+//
+
+#ifndef MYSOFTWARERENDERPIPE_LIGHT_H
+#define MYSOFTWARERENDERPIPE_LIGHT_H
+
+#include "renderer_math.h"
+
+struct direction_light {
+    Vector3f direction;
+    float intensity;
+};
+
+#endif //MYSOFTWARERENDERPIPE_LIGHT_H
