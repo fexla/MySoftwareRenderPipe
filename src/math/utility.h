f6c02eab06c71b96c61c962ce39382170f28dabc
fexla <41383116+fexla@users.noreply.github.com>
2023-10-23 16:25:54 +0800

    clamp function

--- src/math/utility.h
+++ src/math/utility.h
@@ -6,7 +6,7 @@
 #define MYSOFTWARERENDERPIPE_UTILITY_H
 
 #include "array"
-#include "renderer_math.h"
+#include "rm_type_names.h"
 
 using std::array;
 
@@ -14,6 +14,12 @@ inline bool isTriangleClockwise(array<Vector2f, 3> triangle) {
     auto v1 = triangle[1] - triangle[0];
     auto v2 = triangle[2] - triangle[0];
     return v1.cross(v2) < 0;
+}
+
+template<typename DataType>
+inline std::enable_if_t<std::is_arithmetic_v<DataType>, DataType>
+clamp(DataType a, DataType min, DataType max) {
+    return std::min(max, std::max(a, min));
 }
 
 #endif //MYSOFTWARERENDERPIPE_UTILITY_H
